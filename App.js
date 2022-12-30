c4dac44635c5dd7c3783c6f2eca554433d1a79d8
Anand <hyeiamaka06@gmail.com>
2022-12-31 00:02:10 +0530

    word Guess

--- App.js
+++ App.js
@@ -1,1 +1,79 @@
+// all class select
+const inputs = document.querySelector(".inputs");
+const hintTag = document.querySelector(".hint span");
+const guessLeft = document.querySelector(".guess-left span");
+const wrongLetter = document.querySelector(".wrong-letter span");
+const resetBtn = document.querySelector(".reset-btn");
+const typingInput = document.querySelector(".typing-input");
 
+let word, maxGuesses, incorrectLetters = [], correctLetters = []; 
+//function for random word generating
+function randomWord() {
+    document.body.style.backgroundImage = "none";
+    let randomItem = wordList[Math.floor(Math.random() * wordList.length)];
+    word = randomItem.word;
+    maxGuesses = word.length >= 5 ? 8 : 6; //maximum guess can be given 
+    correctLetters = []; 
+    incorrectLetters = [];
+    hintTag.innerText = randomItem.hint;
+    guessLeft.innerText = maxGuesses;
+    wrongLetter.innerText = incorrectLetters;
+    let html = "";
+    for (let i = 0; i < word.length; i++) {
+        html += `<input type="text" disabled>`;
+        inputs.innerHTML = html;
+    }
+}
+randomWord();
+// checking for input
+function initGame(e) {
+    let key = e.target.value.toLowerCase();
+    if (key.match(/^[A-Za-z]+$/) && !incorrectLetters.includes(` ${key}`) && !correctLetters.includes(key)) {
+        if (word.includes(key)) {
+            for (let i = 0; i < word.length; i++) {
+                if (word[i] == key) {
+                    correctLetters += key;
+                    inputs.querySelectorAll("input")[i].value = key;
+                }
+            }
+        } else {
+            maxGuesses--;
+            incorrectLetters.push(` ${key}`);
+        }
+        guessLeft.innerText = maxGuesses;
+        wrongLetter.innerText = incorrectLetters;
+    }
+    typingInput.value = "";
+    setTimeout(() => {
+        if (correctLetters.length === word.length) {
+            startConfetti();
+            document.querySelector(".congrats").innerText="Congratulations You Won !"
+        } else if (maxGuesses < 1) {
+            document.querySelector(".congrats").innerText="Game over! No remaining guesses";
+            for (let i = 0; i < word.length; i++) {
+                inputs.querySelectorAll("input")[i].value = word[i];
+            }
+        }
+    }, 100);
+}
+
+//random color generate
+function genetrateRandomColor() {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    const randomColors = `rgb(${red},${green},${blue})`
+    return randomColors;
+}
+
+// applying event handler
+resetBtn.addEventListener("click", () => {
+    const randomColor = genetrateRandomColor();
+    document.body.style.backgroundColor = randomColor;
+    document.querySelector(".congrats").innerText="";
+    stopConfetti();
+});
+resetBtn.addEventListener("click", randomWord);
+typingInput.addEventListener("input", initGame);
+inputs.addEventListener("click", () => typingInput.focus());
+document.addEventListener("keydown", () => typingInput.focus());
--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# html-css-js-project-boilerplate
+# JS-Project---Calendar
+
+#### By Anand Kumar
+This is a webpage based app Word Guessing Game.
+
+## Table of contents
+
+  - [Description](#description)
+  - [Screenshot](#Screenshot)
+  - [Tech-Stacks-Used](#Tech-Stacks-Used)
+  - [What I learned](#what-i-learned)
+  - [Links](#links) 
+  - [Setup-Installation-Requirements](#Setup-Installation-Requirements)
+  - [Useful resources](#useful-resources)
+  - [Author](#author)
+
+## Overview
+
+### Description
+
+This is web based live Game App of Word Guessing, and this is responsive by nature for big and small devices, with the use of frontend technologies.
+- It is very interesting game & User can enhance his guessing skills.
+- It allows user to guess the word with in limited guessing chance based on word length.
+- Can See When you won the game celebration will be happened in background.
+- User can change the background color randomly by clicking on New Game.
+- When User loses, Game Over will be appear and correct word will be appeared.
+
+### Screenshot
+
+![screenshot](./images/Screenshot.png)
+
+## Tech-Stacks-Used
+   
+- HTML5
+- CSS3
+- JavaScript
+
+### What I learned
+
+While doing this project I have implemented the Javascript functionality of checking right word from list of word and also implemented random color generating functionality, along with I learnt about confetti, different properties of Css. This Project really helped me to polish my css skills as well as JavaScript skills to render something which was running in my mind for combining present, past and future time. I also learnt to implement HTML,CSS,JavaScript to meet design expectation.
+
+
+### Links
+
+- Code URL     : [https://github.com/Anand-kumar96/Word_Guessing_Game)
+- Live Site URL: [https://guess-word-project-by-anand.netlify.app/)
+
+### Setup-Installation-Requirements
+-  Clone this repo to your desktop
+-  Naviagte to the top level of directory
+-  Open index.html in your browser
+
+### Useful resources
+
+- [https://www.w3schools.com/](https://www.w3schools.com/) - This helped me for strengthening the fundamentals of HTML and CSS. it provides all the concept from scratch      level.
+- [https://stackoverflow.com/](https://stackoverflow.com/) - This is an amazing website for solving your doubts and finding any different approaches. I always refer to it to learn different properties of css and javascript.
+
+## Author
+#### Anand Kumar
+- Linkedin - [Anand-kumar96](https://www.linkedin.com/in/anand-kumar96/)
+- GitHub -   [Anand-kumar96](https://github.com/Anand-kumar96)
